06597effe3cf2f0577818af83fcfbf64289f905d
Michael Brydie <michael.brydie@gmail.com>
2024-12-01 09:14:42 +1100

    2021 day 1

---
+++ AdventOfCode/AdventOfCode/Year2021/DepthMeter.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Util;
+
+namespace AdventOfCode.Year2021
+{
+    public class DepthMeter
+    {
+        public int CountIncreases(string puzzleInput)
+        {
+            List<int> depths = GetDepthsFromInput(puzzleInput);
+
+            int increases = 0;
+            for (int i = 1; i < depths.Count; i++)
+            {
+                if (depths[i - 1] < depths[i])
+                {
+                    increases++;
+                }
+            }
+
+            return increases;
+        }
+
+        public object CountIncreasesForSlidingWindow(string puzzleInput)
+        {
+            List<int> depths = GetDepthsFromInput(puzzleInput);
+
+            int increases = 0;
+            for (int i = 0; i < depths.Count - 3; i++)
+            {
+                if (depths[i] + depths[i + 1] + depths[i + 2] < depths[i + 1] + depths[i + 2] + depths[i + 3])
+                {
+                    increases++;
+                }
+            }
+
+            return increases;
+        }
+
+        private static List<int> GetDepthsFromInput(string puzzleInput)
+        {
+            return puzzleInput.Split("\r\n").Select(s => s.Parse<int>()).ToList();
+        }
+    }
+}
