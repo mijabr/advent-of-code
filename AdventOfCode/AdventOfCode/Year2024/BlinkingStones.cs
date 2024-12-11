bdc7a72edbfdf645564ef36e6f5d770d57203515
Michael Brydie <michael.brydie@gmail.com>
2024-12-11 18:41:05 +1100

    2024 day 11

---
+++ AdventOfCode/AdventOfCode/Year2024/BlinkingStones.cs
@@ -0,0 +1,65 @@
+﻿using AdventOfCode.Util;
+
+namespace AdventOfCode.Year2024
+{
+    public class BlinkingStones
+    {
+        public static long CountStonesAfterBlinking(string input, int blinks)
+        {
+            var stones = new Dictionary<long, long>();
+
+            foreach (var stone in input.ParseNumbers<long>())
+            {
+                if (stones.TryGetValue(stone, out var stoneCount))
+                {
+                    stones[stone] = stoneCount + 1;
+                }
+                else
+                {
+                    stones[stone] = 1;
+                }
+            }
+
+            while (blinks > 0)
+            {
+                var stones2 = new Dictionary<long, long>();
+
+                void AddStone(long stone, long count)
+                {
+                    if (stones2.TryGetValue(stone, out var stoneCount))
+                    {
+                        stones2[stone] = stoneCount + count;
+                    }
+                    else
+                    {
+                        stones2[stone] = count;
+                    }
+                }
+
+                foreach (var stone in stones)
+                {
+                    if (stone.Key == 0)
+                    {
+                        AddStone(1, stone.Value);
+                    }
+                    else if ($"{stone.Key}".Length % 2 == 0)
+                    {
+                        var s = $"{stone.Key}";
+                        AddStone(s.Substring(0, s.Length / 2).Parse<long>(), stone.Value);
+                        AddStone(s.Substring(s.Length / 2, s.Length / 2).Parse<long>(), stone.Value);
+                    }
+                    else
+                    {
+                        AddStone(stone.Key * 2024, stone.Value);
+                    }
+                }
+
+                stones = stones2;
+
+                blinks--;
+            }
+
+            return stones.Values.Sum();
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/Year2024/Day11Tests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode.Year2024;
+
+namespace AdventOfCodeTests.Year2024
+{
+    public class Day11Tests
+    {
+        [Test]
+        public void DiskCompactor_GetChecksum_ShouldSolveTestInput()
+        {
+            BlinkingStones.CountStonesAfterBlinking(_puzzleTestInput, 25).Should().Be(55312);
+        }
+
+        [Test]
+        public void DiskCompactor_GetChecksum_ShouldSolveInput()
+        {
+            BlinkingStones.CountStonesAfterBlinking(_puzzleInput, 25).Should().Be(172484);
+        }
+
+        [Test]
+        public void DiskCompactor_GetChecksum_ShouldSolveInputPart2()
+        {
+            BlinkingStones.CountStonesAfterBlinking(_puzzleInput, 75).Should().Be(205913561055242);
+        }
+
+        private const string _puzzleTestInput = "125 17";
+
+        private const string _puzzleInput =
+"20 82084 1650 3 346355 363 7975858 0";
+    }
+}
